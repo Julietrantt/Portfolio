e00b1e7c881f6dfedacd401281a220cbe17a0469
zikster3262 <zikster3262@gmail.com>
2023-08-26 13:38:04 +0200

    add URL shorterer

--- portfolio/app-routing.module.ts
+++ portfolio/app-routing.module.ts
@@ -1,23 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from 'app.component';
-import { PasswordGeneratorComponent } from 'src/app/components/challenges/password-generator/password-generator.component';
-import { MainPageComponent } from 'src/app/components/main-page/main-page.component';
-import { ProfilComponent } from 'src/app/components/main-page/profil/profil.component';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { AppComponent } from 'app.component'
+import { PasswordGeneratorComponent } from 'src/app/components/challenges/password-generator/password-generator.component'
+import { SplitterComponent } from 'src/app/components/challenges/splitter/splitter.component'
+import { MainPageComponent } from 'src/app/components/main-page/main-page.component'
+import { ProfilComponent } from 'src/app/components/main-page/profil/profil.component'
 
+const routes: Routes = [
+  { path: '', redirectTo: '/profil', pathMatch: 'full' },
+  { path: 'profil', component: MainPageComponent },
+  { path: 'password', component: PasswordGeneratorComponent },
+  { path: 'splitter', component: SplitterComponent }
+]
 
- const routes: Routes = [
-    { path: '', redirectTo: '/profil', pathMatch: 'full' },
-    { path: 'profil', component: MainPageComponent },
-    { path: 'password', component: PasswordGeneratorComponent }
-  ]
-  
-  @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-
-export class AppRoutingModule {
- }
-
-  
+export class AppRoutingModule {}
--- portfolio/src/app/components/challenges/splitter/splitter.component.ts
+++ portfolio/src/app/components/challenges/splitter/splitter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-splitter',
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./splitter.component.css']
 })
 export class SplitterComponent implements OnInit {
+  bill: any
+  tip: any
+  tipAmount!: number
+  people: any
+  totalAmount: number | undefined
 
-  constructor() { }
+  constructor () {}
 
-  ngOnInit(): void {
+  ngOnInit (): void {}
+  billVal (e: any) {
+    this.bill = e.target.value
+    console.log(e.target.value)
+    console.log(e)
+    this.amountVal()
   }
 
+  peopleVal (e: any) {
+    this.people = e.target.value
+    console.log(e.target.value)
+    this.amountVal()
+  }
+
+  tipVal (number: number, e?: any) {
+    if (number > 0) {
+      this.tip = number
+    } else if (number == 0) {
+      this.tip = e.target.value
+    }
+    console.log(this.tip)
+    this.amountVal()
+  }
+
+  amountVal () {
+    this.tipAmount = (this.bill * (this.tip / 100)) / this.people
+    this.totalAmount = this.bill / this.people + this.tipAmount
+
+    console.log(this.bill)
+    console.log(this.people)
+    console.log(this.tip / 100)
+    console.log(this.tipAmount)
+    console.log(this.totalAmount)
+  }
 }
