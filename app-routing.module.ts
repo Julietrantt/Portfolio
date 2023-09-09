e3a98cec410edd4ad96f97cd29f29c3064723c89
Julietrantt <1995julietran@gmail.com>
2023-09-09 08:54:37 +0200

    add build

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -14,6 +14,7 @@ import { MultiStepFormComponent } from 'src/app/components/challenges/multi-step
     { path: 'password', component: PasswordGeneratorComponent },
     { path: 'splitter', component: SplitterComponent },
     { path: 'form', component: MultiStepFormComponent },
+    { path: '**', component: MainPageComponent}
   ]
   
 
